80ec48e0e3c2482a31d5092a5db46bd1d04b8185
DOLLA.CHAITRA <168711787+Dollachaitra-0608@users.noreply.github.com>
2024-06-05 17:29:28 +0530

    Add files via upload

---
+++ word_file.py
@@ -0,0 +1,13 @@
+words = ["taxi", "cab", "standing", "cricket",
+         "alarm clock","tool", "banana peel",
+         "flagpole","money","hardwork",
+         "ballpoint pen","sunburn", "wedding",
+         "ring" ,"spy", "aunt","puzzle piece",
+         "pawn","astronaut", "tennis" ,"shoes",
+         "trip","outer space","batteries",
+         "doghouse","campsite","plumber",
+         "throne","logo", "mute","stove",
+         "windmill", "nightmare","wristwatch",
+         "eat","matchstick","sprinkler",
+         "living room","laugh","elf,headache",
+         "Internet","phone"]
